98ada654a9a4e0bce86de5e63c5341b593a1c6e8
unknown <krishanb281988@gmail.com>
2024-07-30 12:52:42 +0530

    tt

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -1,7 +1,13 @@
 resource "aws_instance" "web" {
-    instance_type = "t3.micro"
+  ami           = i-03159e9a0cae186c4
+  instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+output "web" {
+    value = aws_instance.web.private_dns
+  
 }
