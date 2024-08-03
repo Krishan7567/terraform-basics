bd16917302852c46e8e9df030ce67708ec6539ca
unknown <krishanb281988@gmail.com>
2024-08-03 12:07:21 +0530

    tt

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f75a13ad2e340a58"
-  instance_type = "t2.micro"
+  ami                       = "ami-0f75a13ad2e340a58"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = aws_security_group.allow_ssh.id
 
   tags = {
     Name = "testing"
