d71a1358bd15c8419e84303b449c6a940cc36b76
unknown <krishanb281988@gmail.com>
2024-08-03 12:00:07 +0530

    tt

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -23,14 +23,14 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic and all outbound traffic"
 
-  ingress = {
+  ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-   egress = {
+   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "tcp"
