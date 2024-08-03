65d44d71702ac57381dcfb264480fd50a9f1d451
unknown <krishanb281988@gmail.com>
2024-08-03 11:57:45 +0530

    tt

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -16,3 +16,28 @@ output "test" {
     value = aws_instance.web.public_ip
   
 }
+
+### SG creation
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic and all outbound traffic"
+
+  ingress = {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   egress = {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
