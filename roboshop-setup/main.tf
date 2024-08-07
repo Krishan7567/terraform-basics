f0aa857c3da2653549b42293fa7997c38d31a6ac
unknown <krishanb281988@gmail.com>
2024-08-07 15:15:48 +0530

    tt

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -2,3 +2,18 @@ module "mongodb" {
  source      =   "./ec2"
  COMPONENT   =   "mongodb"
  }
+
+ module "redis" {
+ source      =   "./ec2"
+ COMPONENT   =   "redis"
+ }
+
+ module "test" {
+ source      =   "./ec2"
+ COMPONENT   =   "test"
+ }
+
+ module "master" {
+ source      =   "./ec2"
+ COMPONENT   =   "master"
+ }
